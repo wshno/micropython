a6efaed6be9906db165e6bdd42c38f91ad34aa77
Hans Christian Winther-Sorensen <hc@wsh.no>
2023-06-27 23:55:47 +0200

    Fix esp32 nodemcu idf5 compatibility issue

--- ports/esp32/boards/NODEMCU32S/mpconfigboard.cmake
+++ ports/esp32/boards/NODEMCU32S/mpconfigboard.cmake
@@ -1,7 +1,8 @@
+set(IDF_TARGET esp32)
+
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.ble
 )
-if(NOT MICROPY_FROZEN_MANIFEST)
-    set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/manifest.py)
-endif()
+
+set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
